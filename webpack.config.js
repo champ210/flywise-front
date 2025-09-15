83e5bab1d657b14924ab154e70778c6e10de5666
champ210 <hamza.riane@gmail.com>
2025-09-15 10:45:44 +0100

    feat: Integrate dotenv-webpack for environment variables
    This change introduces `dotenv-webpack` to manage environment-specific configurations. The `index.html` file is updated to comment out the import map, indicating a shift towards using environment variables for CDN paths or other configurations. The webpack configuration is updated to conditionally load `.env` or `.env.dev` files based on the build mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: argv.mode || 'development',
+    entry: './index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
     },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
+    devtool: isProduction ? false : 'source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
       },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, './'),
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      alias: {
+        '@': path.resolve(__dirname, './'),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new Dotenv({
+        path: isProduction ? './.env' : './.env.dev',
+      }),
+    ],
+  };
 };
